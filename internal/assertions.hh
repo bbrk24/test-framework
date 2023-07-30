650320c9bb5df2173a13d5c2b3f7564ef365de9e
William Baker <bbrk24@gmail.com>
2023-07-30 02:16:51 -0400

    Add documentation

--- internal/assertions.hh
+++ internal/assertions.hh
@@ -1,5 +1,20 @@
 #pragma once
 
+/**
+ * Indicate that a test has failed.
+ * @param[in] reason Optional string describing why the test failed.
+ */
 [[noreturn]] void test_fail(const char* reason = "test_fail() called");
+
+/**
+ * Indicate that a test should be skipped.
+ * @param[in] reason Optional string describing why the test is skipped.
+ */
 [[noreturn]] void test_skip(const char* reason = "test_skip() called");
+
+/**
+ * Check that the condition is true.
+ * @param condition  The condition to test.
+ * @param[in] reason Optional string to be shown if the assertion fails.
+ */
 void test_assert(bool condition, const char* reason = "test assertion failed");
--- test_framework.hh
+++ test_framework.hh
@@ -4,7 +4,18 @@
 #include "internal/test_case.hh"
 #include "internal/test_group.hh"
 
+/**
+ * Declare a group of tests. Invocation of this macro must be followed by an
+ * initializer list of test cases.
+ * @param name The name of the test group. Must be a valid identifier.
+ */
 #define testgroup(name) const test::test_group name = test::test_name(#name) +=
+
+/**
+ * Define a single test. Invocation of this macro must be followed by a function
+ * body.
+ * @param name The name of the test case.
+ */
 #define testcase(name) test::test_name(#name) + []()
 
 #define _TEST_THROWS_(call, message, ...) \
@@ -17,6 +28,13 @@
         } \
         test_assert(__thrown, message); \
     } while (false)
+
+/**
+ * Check that a statement or expression throws when run.
+ * @param call        The statement or expression to run.
+ * @param[in] message Optional string to be shown if the expression does not
+ *                    throw.
+ */
 #define test_throws(...) \
     _TEST_THROWS_( \
         __VA_ARGS__, \
